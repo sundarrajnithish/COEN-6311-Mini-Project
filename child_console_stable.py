d62e8be0c5bc2810e3584d6447c207f02d7f1075
Nitish Sundarraj <sundarrajnithish@gmail.com>
2022-10-18 11:10:19 -0400

    Stable-v1

---
+++ child_console_stable.py
@@ -0,0 +1,78 @@
+import pickle
+from backend_stable import Family_Wallet
+import time
+
+user1 = ""
+logged_in = False
+
+
+# Welcome page for the kids wallet access
+def welcome():
+    global user1
+    global logged_in
+    print("Welcome to the Family Bank Wallet (Kids Edition)! \nType the corresponding numbers for navigating the menu.")
+    users = input("\n Type your name number below for Login: \n 1. Jake Williams \n 2. Tony Williams \n 3. Mickey "
+                  "Williams \n 4. Sofia Williams \n 5. Mia Williams \n 6. Shakira Williams \n 7. Amber Williams \n 8. "
+                  "Ambani Williams \n Your choice: ")
+    logged_in = True
+    if users == '1':
+        user1 = Family_Wallet.Kid('jake')
+    if users == '2':
+        user1 = Family_Wallet.Kid('tony')
+    if users == '3':
+        user1 = Family_Wallet.Kid('mickey')
+        user1.transaction_reset()
+        print("This is the output it provides", user1.overpay_amount[user1.name])
+        print("This is the whole list", user1.overpay_amount)
+    if users == '4':
+        user1 = Family_Wallet.Kid('sofia')
+        user1.transaction_reset()
+        print("This is the output it provides", user1.overpay_amount[user1.name])
+        print("This is the whole list", user1.overpay_amount)
+    if users == '5':
+        user1 = Family_Wallet.Kid('mia')
+    if users == '6':
+        user1 = Family_Wallet.Kid('shakira')
+    if users == '7':
+        user1 = Family_Wallet.Kid('amber')
+    if users == '8':
+        user1 = Family_Wallet.Kid('ambani')
+
+
+def balance_refresh(name):
+    with open('data/overpay_amount.pickle', 'rb') as handle:
+        my_balance = pickle.load(handle)
+        #print("Yes, balance is updated")
+        #print("balance list is ", my_balance)
+    return my_balance[name]
+
+
+def login():
+    global user
+    user1.transaction_reset()
+    my_wallet = Family_Wallet(user1.name)
+    if not my_wallet.blocked_user_flag:
+        bal = balance_refresh(user1.name)
+
+        task = input("Welcome {}! \n You daily balance is ${} \n What would you like to do? \n 1. Pay "
+                     " \n 2. Logout".format(user1.name, bal))  # user1.overpay_amount[user1.name]
+        if task == '1':
+            user1.transaction(shop_name=input("Enter the merchant name: "), amount=input("Enter the amount: $"))
+            login()
+        if task == '2':
+            print("Loading...")
+            time.sleep(1)
+            welcome()
+        if task == 'print':
+            print(user1.transaction_list)
+            login()
+    else:
+        print("You have been blocked from using the wallet \n Request Dad to Unblock")
+        welcome()
+
+
+while True:
+    if logged_in:
+        login()
+    else:
+        welcome()
---
+++ parent_console_stable.py
@@ -0,0 +1,95 @@
+# import os
+import pickle
+import time
+from backend_stable import Family_Wallet
+
+sarah_williams = Family_Wallet.Mom()
+williams_hemisphere = Family_Wallet.Dad()
+
+
+# Welcome screen for the parent console
+def welcome():
+    print("Welcome to the Family Bank Wallet! \nType the corresponding numbers for navigating the menu.")
+    users = input("\n Type your name number below for Login: \n 1. Sarah Williams (Mom)\n 2. Williams Hemisphere ("
+                  "Dad). \n Your choice: ")
+    return users
+
+
+dad_balance = 0
+mom_balance = 0
+
+
+def balance_refresh():
+    global dad_balance
+    global mom_balance
+    with open('data/dad_balance.pickle', 'rb') as handle:
+        dad_balance = pickle.load(handle)
+    with open('data/mom_balance.pickle', 'rb') as handle:
+        mom_balance = pickle.load(handle)
+    #print(mom_balance,"log")
+
+
+# Menu after logging in
+def menu(users):
+    if users == '1' or users == '2':
+        balance_refresh()
+        if users == '1':
+            print("You account balance is $", mom_balance)
+        if users == '2':
+            print("You account balance is $", dad_balance)
+        menus = input("Choose any option below: \n 1. Withdraw (Bank) \n 2. Deposit (Bank) \n 3. Family Wallet "
+                      "\n 4. Logout \n Your choice: ")
+        return menus
+
+
+# Menu actions for mom
+def mom(user_mom):
+    mom_wallet = Family_Wallet('mom')
+    menus = menu(user_mom)
+    balance_refresh()
+    if menus == '1' and user_mom == '1':
+        sarah_williams.withdraw((input("How much would you like to withdraw? $ ")))
+        mom(user_mom)
+    if menus == '2' and user_mom == '1':
+        sarah_williams.deposit((input("How much would you like to deposit? $ ")))
+        mom(user_mom)
+    if menus == '3' and user_mom == '1':
+        mom_wallet.parent_wallet_access()
+        mom(user_mom)
+    if menus == '4' and user_mom == '1':
+        time.sleep(1)
+        print("You have successfully logged out! \n\n\n")
+
+
+# Menu actions for dad
+def dad(user_dad):
+    dad_wallet = Family_Wallet('dad')
+    menus = menu(user_dad)
+    if menus == '1' and user_dad == '2':
+        williams_hemisphere.withdraw((input("How much would you like to withdraw? $ ")))
+        dad(user_dad)
+    if menus == '2' and user_dad == '2':
+        williams_hemisphere.deposit(input("How much would you like to deposit? $ "))
+        dad(user_dad)
+    if menus == '3' and user_dad == '2':
+        dad_wallet.parent_wallet_access()
+        dad(user_dad)
+    if menus == '4' and user_dad == '2':
+        time.sleep(1)
+        print("You have successfully logged out! \n\n\n")
+
+
+# While loop to perform all the tasks repeatedly
+
+while 1:
+    # os.system('cls')
+    time.sleep(1)
+    user = welcome()
+    if user == '1':
+        mom(user)
+    elif user == '2':
+        dad(user)
+    elif user == 'close':
+        exit()
+    else:
+        print("Invalid Entry! Try Again!")
